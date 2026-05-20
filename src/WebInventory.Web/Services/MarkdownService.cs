4f269a5b3bcacb78d39d382a7782e3b3783bbe1f
Narek <narekkarapetyan015@gmail.com>
2026-05-20 21:41:52 +0400

    Dev | Markdown for Inventory added

--- src/WebInventory.Web/Services/MarkdownService.cs
+++ src/WebInventory.Web/Services/MarkdownService.cs
@@ -9,8 +9,13 @@ public class MarkdownService
         .UseAdvancedExtensions()
         .Build();
 
-    public string ToHtml(string markdown)
+    public string ToHtml(string? markdown)
     {
+        if (string.IsNullOrWhiteSpace(markdown))
+        {
+            return string.Empty;
+        }
+
         return Markdown.ToHtml(markdown, _pipeline);
     }
 }
